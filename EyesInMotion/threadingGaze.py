922cde06c450f9aedb28913e310dee9313879d73
Framor81 <francisco.moralespuente2022@gmail.com>
2024-05-21 14:19:47 -0700

    Finished Threading

--- EyesInMotion/threadingGaze.py
+++ EyesInMotion/threadingGaze.py
@@ -1,5 +1,7 @@
 import math
+import queue
 import sys
+import threading
 import time
 
 import cv2
@@ -8,17 +10,22 @@ import numpy as np
 
 # Define the fixed window size
 WINDOW_SIZE = (200, 100)
+# Shared queue to hold frames from multiple cameras
+frame_queue = queue.Queue()
+# Stop Event to signal when to stop the threads.
+stop_event = threading.Event()
 
-def create_cam():
+window_names = {}
+
+def create_cam(camID):
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(r'EyesInMotion\shape_predictor_68_face_landmarks.dat')
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(camID)
     # Exit program if we can't open the camera
     if not cap.isOpened():
         print("Error: Could not open camera.")
         exit()
     return detector, predictor, cap
-
 
 def facial_landmarks(predictor, face, landmarks, lB, uB):
     """
@@ -33,7 +40,6 @@ def facial_landmarks(predictor, face, landmarks, lB, uB):
     eye_center = np.mean(eye_points, axis=0).astype(int)
     return eye_points, eye_center
 
-
 def draw_landmarks(frame, eye_points):
     # Draw the landmarks on frame
     for (x,y) in eye_points:
@@ -45,7 +51,6 @@ def eye_orientation(left_eye_center: list[tuple], right_eye_center: list[tuple])
     # Calculate the center between the two eyes
     eyes_center = ((left_eye_center[0] + right_eye_center[0]) / 2.0, (left_eye_center[1] + right_eye_center[1]) / 2.0)
     return angle, eyes_center
-
 
 def orient_eyes(frame, detector, eyes_center, angle):
     # Get the rotation matrix for the affine transformation to align the eyes horizontally.
@@ -59,7 +64,6 @@ def orient_eyes(frame, detector, eyes_center, angle):
     # Detect faces in the rotated image
     dets_rotated = detector(gray_rotated)
     return rotated_frame, gray_rotated, dets_rotated
-
 
 def calculate_fixed_bounding_box(center, frame_shape, window_size):
     """
@@ -84,7 +88,6 @@ def calculate_fixed_bounding_box(center, frame_shape, window_size):
     y_max = int(min(frame_shape[0], y_center + half_height))
 
     return x_min, y_min, x_max, y_max
-
 
 def draw_eye_crosshair(frame, eye_points, track_blinking=False, opacity=1.0, color=(0, 255, 0)):
     """
@@ -113,7 +116,6 @@ def draw_eye_crosshair(frame, eye_points, track_blinking=False, opacity=1.0, col
     vertical_line = cv2.line(copy, (x_mid, y_min), (x_mid, y_max), color, 1)
     cv2.addWeighted(copy, opacity, frame, 1 - opacity, 0, frame)
 
-
     if(track_blinking):
         if ((x_max - x_min) / (y_max - y_min) > 4.75):
             print("BLINKING")
@@ -121,7 +123,6 @@ def draw_eye_crosshair(frame, eye_points, track_blinking=False, opacity=1.0, col
             return True
         else:
             return False
-
 
 def cropped_points(x_min, y_min, x_max, y_max, eye_points_rotated: list[tuple]):
     # Calculate the scaling factors
@@ -175,71 +176,111 @@ def mask_eyes(frame, left_eye_points, right_eye_points):
     # final_result is the masked eyes, result_with_eyes is just the eyes without the mask 
     return final_result, result_with_eyes, black_around_eyes
 
+class camThread(threading.Thread):
+    def __init__(self, camID):
+        threading.Thread.__init__(self)
+        self.camID = camID
+        self.detector, self.predictor, self.cam = create_cam(camID)
 
-try:
-    detector, predictor, cap = create_cam()
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        if not ret:
-            print("Failed to grab frame")
-            break
+    def run(self):
+        if not self.cam.isOpened():
+            print(f"Error Opening Camera {self.camID}")
+            return
+        
+        print(f"Camera {self.camID} warming up...")
+        time.sleep(4)  # Allow the camera to warm up
+        
+        while not stop_event.is_set():
+            eye_tracking(self.detector, self.predictor, self.cam, self.camID)
 
-        # Convert the frame to grayscale as HOG detector works on grayscale images
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def displayFrames():
+    while not stop_event.is_set():
+        if not frame_queue.empty():
+            name, camID, frame = frame_queue.get()
+            if name not in window_names:
+                cv2.namedWindow(name)
+                window_names[name] = camID
+            cv2.imshow(name, frame)
 
-        # Detect faces in the image
-        dets = detector(gray)
-
-        for d in dets:
-            left_eye_points, left_eye_center = facial_landmarks(predictor, gray, d, 36, 42)
-            right_eye_points, right_eye_center = facial_landmarks(predictor, gray, d, 42, 48)
-
-            angle, eyes_center = eye_orientation(left_eye_center, right_eye_center)
-
-            rotated_frame, gray_rotated, dets_rotated = orient_eyes(frame, detector, eyes_center, angle)
-            
-            for d_rotated in dets_rotated:
-                eye_points_rotated, _ = facial_landmarks(predictor, gray_rotated, d_rotated, 36, 48)
-                x_min, y_min, x_max, y_max = calculate_fixed_bounding_box(eyes_center, rotated_frame.shape, WINDOW_SIZE)
-
-                # Crop the frame to the bounding box
-                cropped_frame = rotated_frame[y_min:y_max, x_min:x_max]
-
-                # Resize the cropped frame to fit the fixed window size
-                resized_frame = cv2.resize(cropped_frame, WINDOW_SIZE)
-                    
-                eye_points_cropped = cropped_points(x_min, y_min, x_max, y_max, eye_points_rotated)
-
-                # draw_landmarks(resized_frame, eye_points_cropped)
-
-                draw_eye_crosshair(resized_frame, eye_points_cropped[:6], False, 0.5)
-                draw_eye_crosshair(resized_frame, eye_points_cropped[6:], False, 0.5)
-
-                # Show the resized frame
-                cv2.imshow('Eyes', resized_frame)
-                _, masked_eyes, _ = mask_eyes(resized_frame, eye_points_cropped[:6], eye_points_cropped[6:])
-                cv2.imshow('Mask Eye', masked_eyes)
-
-        masked_frame, _, _ = mask_eyes(frame, left_eye_points, right_eye_points)
-        _, _, inverse_masked_frame = mask_eyes(frame, left_eye_points, right_eye_points)
-
-
-        # Crosshair for actual image
-        draw_eye_crosshair(frame, left_eye_points, True, 0.5)
-        draw_eye_crosshair(frame, right_eye_points, True, 0.5)
-
-        draw_eye_crosshair(masked_frame, left_eye_points, True, 0.8, (0, 0, 255))
-        draw_eye_crosshair(masked_frame, right_eye_points, True, 0.8, (0, 0, 255))
-
-        cv2.imshow('Frame', frame)
-        cv2.imshow('Mask', masked_frame)
-        cv2.imshow('Inverse Mask', inverse_masked_frame)
-
-        # Break the loop on 'q' key press
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            stop_event.set()
             break
-finally:
-    # When everything is done, release the capture
-    cap.release()
+
     cv2.destroyAllWindows()
+
+def eye_tracking(detector, predictor, cap, camID):
+    try:
+        while not stop_event.is_set():
+            ret, frame = cap.read()
+            if not ret:
+                print(f"Failed to grab frame from camera {camID}")
+                break
+
+            # Convert the frame to grayscale as HOG detector works on grayscale images
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Detect faces in the image
+            dets = detector(gray)
+
+            # if len(dets) == 0:
+            #     frame_queue.put((f'Frame {camID}', camID, frame))
+            #     continue
+
+            for d in dets:              
+                left_eye_points, left_eye_center = facial_landmarks(predictor, gray, d, 36, 42)
+                right_eye_points, right_eye_center = facial_landmarks(predictor, gray, d, 42, 48)
+
+                angle, eyes_center = eye_orientation(left_eye_center, right_eye_center)
+
+                rotated_frame, gray_rotated, dets_rotated = orient_eyes(frame, detector, eyes_center, angle)
+                
+                for d_rotated in dets_rotated:
+                    eye_points_rotated, _ = facial_landmarks(predictor, gray_rotated, d_rotated, 36, 48)
+                    x_min, y_min, x_max, y_max = calculate_fixed_bounding_box(eyes_center, rotated_frame.shape, WINDOW_SIZE)
+
+                    # Crop the frame to the bounding box
+                    cropped_frame = rotated_frame[y_min:y_max, x_min:x_max]
+
+                    # Resize the cropped frame to fit the fixed window size
+                    resized_frame = cv2.resize(cropped_frame, WINDOW_SIZE)
+                        
+                    eye_points_cropped = cropped_points(x_min, y_min, x_max, y_max, eye_points_rotated)
+
+                    # draw_landmarks(resized_frame, eye_points_cropped)
+
+                    draw_eye_crosshair(resized_frame, eye_points_cropped[:6], False, 0.5)
+                    draw_eye_crosshair(resized_frame, eye_points_cropped[6:], False, 0.5)
+
+                    # Show the resized frame
+                    _, masked_eyes, _ = mask_eyes(resized_frame, eye_points_cropped[:6], eye_points_cropped[6:])
+                    # frame_queue.put((f'Eyes {camID}', camID, resized_frame))
+                    frame_queue.put((f'Mask Eye {camID}', camID, masked_eyes))
+
+            masked_frame, _, _ = mask_eyes(frame, left_eye_points, right_eye_points)
+            _, _, inverse_masked_frame = mask_eyes(frame, left_eye_points, right_eye_points)
+
+            # Crosshair for actual image
+            draw_eye_crosshair(frame, left_eye_points, True, 0.5)
+            draw_eye_crosshair(frame, right_eye_points, True, 0.5)
+
+            draw_eye_crosshair(masked_frame, left_eye_points, True, 0.8, (0, 0, 255))
+            draw_eye_crosshair(masked_frame, right_eye_points, True, 0.8, (0, 0, 255))
+
+            # frame_queue.put((f'Frame {camID}', camID, frame))
+            frame_queue.put((f'Mask {camID}', camID, masked_frame))
+            # frame_queue.put((f'Inverse Mask {camID}', camID, inverse_masked_frame))
+
+    finally:
+        cap.release()
+
+# Create and start the worker threads
+thread1 = camThread(0)
+thread1.start()
+
+# Run the display function in the main thread
+displayFrames()
+
+# Wait for the worker threads to finish
+thread1.join()
+
+print("Application closed.")
